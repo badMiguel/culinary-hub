1332efdb659afd35ac192d17be97ba268bbb3a20
MiguelCDU <s365958@students.cdu.edu.au>
2024-03-25 22:50:21 +0930

    .

--- Miguel Badayos/Forms_Sprint 2/input.js
+++ Miguel Badayos/Forms_Sprint 2/input.js
@@ -5,13 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
             let inputId = input.getAttribute('id');
             let label = document.querySelector(`label[for="${inputId}"]`);            
             let indicator = label.nextElementSibling
-            if (indicator && input.value !== '') {
-                indicator.style.visibility = 'hidden'; // Hide indicator when input is filled
+            if (input.value !== '') {
+                indicator.style.visibility = 'hidden';
             } else{
                 indicator.style.visibility = 'visible';
             }
         });
     });
+    const form = document.getElementById("form")
+    const firstName = document.getElementById("firstName")
+    const lastName = document.getElementById("lastName")
+    const email = document.getElementById("email")
+    const contactNum = document.getElementById("contactNum")
+    const recipeName = document.getElementById("recipeName")
+    const description = document.getElementById("description")
+    const cookingTime = document.getElementById("cookingTime")
+    const difficulty = document.getElementById("difficulty")
+    const ingredients = document.getElementById("ingredients")
+    const instructions = document.getElementById("instructions")
+    form.addEventListener('submit', function(prevent){
+        let errorFname=[]
+        if (firstName.value =='' || firstName.value==null){
+            
+        };
+        if (errorMessage.length> 0){
+            prevent.preventDefault()
+        };
+    })
 });
 
 
