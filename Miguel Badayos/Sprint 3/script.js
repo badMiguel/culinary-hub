619fed26cb91afb318a020b22b9b0cd8d45b89b1
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-04 19:07:15 +0930

    sprint 3

--- Miguel Badayos/Sprint 3/script.js
+++ Miguel Badayos/Sprint 3/script.js
@@ -1,61 +1,64 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const heartButton = document.querySelector('.heart');
-    const heartColor = document.querySelector('.heart-color');
-    const heartNumberDisplay = document.querySelector('.heart-number');
+    const heartButton = document.querySelectorAll('.heart');
+    let heartIdNumber = 0
 
-    let heartNumber = 1233
-    heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-    
-    heartButton.addEventListener('click', function(){
-        heartNumberDisplay.textContent = heartNumber.toLocaleString();    
-        if (!heartColor.style.fill || heartColor.style.fill === 'var(--bg-secondary)'){
-            heartColor.style.fill = 'rgb(170, 26, 26)';
-            heartColor.style.stroke = 'rgb(170, 26, 26)';
+    heartButton.forEach(heartButton => {
+        heartIdNumber += 1
         
-            heartNumber += 1;
-            heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-
-        } else {
-            heartColor.style.fill = 'var(--bg-secondary)';
-            heartColor.style.stroke = 'black';
+        let heartIdString = heartIdNumber.toString()
+        let heartIdLikes = 'heartLikes-'+ heartIdString
+        let heartIdColor = 'heartColor-' + heartIdString
+        
+        const heartColor = document.getElementById(heartIdColor);
+        const heartNumberDisplay = document.getElementById(heartIdLikes);
+        
+        let heartNumber = randomInt(1,1000)
+        heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
+        
+        heartButton.addEventListener('click', function(){
+            heartNumberDisplay.textContent = heartNumber.toLocaleString();    
+            if (!heartColor.style.fill || heartColor.style.fill === 'var(--bg-secondary)'){
+                heartColor.style.fill = 'rgb(170, 26, 26)';
+                heartColor.style.stroke = 'rgb(170, 26, 26)';
             
-            heartNumber -= 1;
-            heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-        };
+                heartNumber += 1;
+                heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
+    
+            } else {
+                heartColor.style.fill = 'var(--bg-secondary)';
+                heartColor.style.stroke = 'black';
+                
+                heartNumber -= 1;
+                heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
+            };
+        });
     });
 
-    const bookmarkButton = document.querySelector('.bookmark');
-    const bookmarkColor = document.querySelector('.bookmark-color');
+    const bookmarkButton = document.querySelectorAll('.bookmark');
+    let bookmarkIdNumber = 0
 
-    bookmarkButton.addEventListener('click', function(){
-        if (!bookmarkColor.style.fill || bookmarkColor.style.fill ==='var(--bg-secondary)'){
-            bookmarkColor.style.fill = 'rgb(230, 230, 42)';
-            bookmarkColor.style.stroke = 'rgb(230, 230, 42)';
-        } else {
-            bookmarkColor.style.fill = 'var(--bg-secondary)';
-            bookmarkColor.style.stroke = 'black';
-        }
-    });
+    bookmarkButton.forEach(bookmarkButton => {
+        bookmarkIdNumber += 1;
+        bookmarkIdString = bookmarkIdNumber.toString();
+        bookmarkId = 'bookmark-' + bookmarkIdString;
+        bookmarkIdColor = 'bookmarkColor-' + bookmarkIdString;
+        
+        const bookmarkColor = document.getElementById(bookmarkIdColor);
 
-    const viewMore = document.querySelector('.view-more');
-    const hiddenContent = document.querySelector('.hidden-content');
-    const card = document.querySelector('.card');
-
-    viewMore.addEventListener('click', function(){
-        const cardLeftEdge = card.getBoundingClientRect();
-        const cardLeft = cardLeftEdge.left;
-
-        const buttonLeftEdge = viewMore.getBoundingClientRect();
-        const buttonLeft = buttonLeftEdge.right;
-
-        let windowWidth = window.innerWidth
-
-        let cardTranslate = cardLeft*.8
-        let buttonTranslate = (windowWidth-buttonLeft)*.8
-
-        card.style.transform = `translateX(-${cardTranslate}px)`;
-        card.style.transition= 'transform 0.5s ease';
-        viewMore.style.transform = `translateX(${buttonTranslate}px)`;
-        viewMore.style.transition= 'transform 0.5s ease';
+        bookmarkButton.addEventListener('click', function(){
+            if (!bookmarkColor.style.fill || bookmarkColor.style.fill ==='var(--bg-secondary)'){
+                bookmarkColor.style.fill = 'rgb(230, 230, 42)';
+                bookmarkColor.style.stroke = 'rgb(230, 230, 42)';
+            } else {
+                bookmarkColor.style.fill = 'var(--bg-secondary)';
+                bookmarkColor.style.stroke = 'black';
+            }
+        });
     });
 });
+
+function randomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
