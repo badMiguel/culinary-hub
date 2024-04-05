6cfe70ed9827bd3b44457392ebaeb342446f1015
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-05 09:58:39 +0930

    sprint 3

--- Miguel Badayos/Sprint 3/script.js
+++ Miguel Badayos/Sprint 3/script.js
@@ -1,3 +1,5 @@
+let heartNumber = randomInt(1,1000)
+
 document.addEventListener('DOMContentLoaded', function(){
     const heartButton = document.querySelectorAll('.heart');
     let heartIdNumber = 0
@@ -11,26 +13,11 @@ document.addEventListener('DOMContentLoaded', function(){
         
         const heartColor = document.getElementById(heartIdColor);
         const heartNumberDisplay = document.getElementById(heartIdLikes);
-        
-        let heartNumber = randomInt(1,1000)
+               
         heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-        
+
         heartButton.addEventListener('click', function(){
-            heartNumberDisplay.textContent = heartNumber.toLocaleString();    
-            if (!heartColor.style.fill || heartColor.style.fill === 'var(--bg-secondary)'){
-                heartColor.style.fill = 'rgb(170, 26, 26)';
-                heartColor.style.stroke = 'rgb(170, 26, 26)';
-            
-                heartNumber += 1;
-                heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-    
-            } else {
-                heartColor.style.fill = 'var(--bg-secondary)';
-                heartColor.style.stroke = 'black';
-                
-                heartNumber -= 1;
-                heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
-            };
+            changeColor(heartColor, 'rgb(170, 26, 26)', 'heart', heartNumberDisplay);
         });
     });
 
@@ -46,13 +33,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const bookmarkColor = document.getElementById(bookmarkIdColor);
 
         bookmarkButton.addEventListener('click', function(){
-            if (!bookmarkColor.style.fill || bookmarkColor.style.fill ==='var(--bg-secondary)'){
-                bookmarkColor.style.fill = 'rgb(230, 230, 42)';
-                bookmarkColor.style.stroke = 'rgb(230, 230, 42)';
-            } else {
-                bookmarkColor.style.fill = 'var(--bg-secondary)';
-                bookmarkColor.style.stroke = 'black';
-            }
+            changeColor(bookmarkColor, 'rgb(230, 230, 42)', 'bookmark')
         });
     });
 });
@@ -61,4 +42,22 @@ function randomInt(min,max){
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function changeColor(element, color, button, likes){
+    if (!element.style.fill || element.style.fill ==='var(--bg-secondary)'){
+        element.style.fill = color;
+        element.style.stroke = color;
+        if (button === 'heart'){
+            heartNumber += 1;
+            likes.textContent = heartNumber.toLocaleString(); 
+        }
+    } else{
+        element.style.fill = 'var(--bg-secondary)';
+        element.style.stroke = 'black'; 
+        if (button === 'heart'){
+            heartNumber -= 1;
+            likes.textContent = heartNumber.toLocaleString(); 
+        }
+    }  
 }
