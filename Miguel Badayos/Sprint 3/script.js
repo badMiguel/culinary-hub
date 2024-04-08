9d46eb35e646f5dc3bf807bc1fc7d65ac94b6b9d
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-08 21:43:13 +0930

    Added share button. Clicking opens a pop up with different share options. Also allows to copy url of the recipe. Major improvement on the responsiveness. Have different sizes for small, medium, and large screens

--- Miguel Badayos/Sprint 3/script.js
+++ Miguel Badayos/Sprint 3/script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+    // shorten the description to fit into the card
+    const imageDescription = document.querySelectorAll('.image_description')  
+    imageDescription.forEach(imageDescription => {
+        const imageDescriptionContent = imageDescription.textContent
+        const stringLength = imageDescriptionContent.length
+
+        let  descriptionSplit = imageDescriptionContent.split('');
+
+        if (stringLength > 150){
+            let newDescription = ''
+            for (let letter = 0; letter<=129; letter++){
+                newDescription += descriptionSplit[letter]
+            }
+            newDescription += '...'
+            imageDescription.textContent = newDescription
+        }
+    });
+
+    // adds a amount of like when recipe is liked - change  color of button
     const heartButton = document.querySelectorAll('.heart');
     let heartIdNumber = 0
     
@@ -15,7 +34,6 @@ document.addEventListener('DOMContentLoaded', function(){
                
         heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
 
-        console.log(heartNumber)
         heartButton.addEventListener('click', function(){
             likeChange = changeColor(heartColor, 'rgb(170, 26, 26)', 'heart')
             heartNumber += likeChange
@@ -23,6 +41,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     });
 
+    // change color of button when clicked
     const bookmarkButton = document.querySelectorAll('.bookmark');
     let bookmarkIdNumber = 0
 
@@ -37,7 +56,60 @@ document.addEventListener('DOMContentLoaded', function(){
             changeColor(bookmarkColor, 'rgb(230, 230, 42)', 'bookmark')
         });
     });
+
+    // open share menu pop up
+    const shareButton = document.querySelectorAll('.share-button')
+    const copyButton = document.querySelector('.copy-button')
+    let linkIdNumber = 0
+    shareButton.forEach(shareButton => {
+        linkIdNumber += 1
+        let linkIdString = linkIdNumber.toString()
+        let linkId = 'link-' + linkIdString
+
+        let linkElement = document.getElementById(linkId)
+        let recipeLink = linkElement.getAttribute('href')
+
+        const shareLink = document.querySelector('.share-link')
+
+        shareButton.addEventListener('click', function(){
+            sharePopup.forEach(sharePopup => {
+                sharePopup.style.visibility = 'visible'
+                sharePopup.style.opacity = 1
+            });
+
+            shareLink.value = recipeLink
+            copyButton.addEventListener('click', function(){
+                copyLink(recipeLink)
+            })
+        });
+    });
+   
+    // close share menu pop up
+    const exitShare = document.querySelectorAll('.hide-bg-content, .close-share')
+    const sharePopup = document.querySelectorAll('.share-popup, .hide-bg-content');
+    exitShare.forEach(exitShare => {
+        exitShare.addEventListener('click', function(){
+            sharePopup.forEach(sharePopup => {
+                sharePopup.style.opacity = 0
+                setTimeout(() => {
+                    sharePopup.style.visibility = 'hidden' 
+                }, 200);
+            });
+        })
+    });
 });
+
+// copies the link of the target url
+function copyLink(link){
+    navigator.clipboard.writeText(link)
+    
+    const notifyCopy = document.querySelector('.copied-clipboard')
+    notifyCopy.style.opacity = 1
+
+    setTimeout(() => {
+        notifyCopy.style.opacity = 0
+    }, 3000);
+}
 
 // just generates a random number of likes
 function randomInt(min,max){
