29481688a9921235dc4996a4cd437a05ce2802ac
MiguelCDU <s365958@students.cdu.edu.au>
2024-04-28 14:39:37 +0930

    Created a JS function that duplicates cards depending on how many recipe are in the JSON file. displays these information in the cards

--- script.js
+++ script.js
@@ -1,31 +1,130 @@
-document.addEventListener('DOMContentLoaded', function(){
-    // adds a amount of like when recipe is liked - change  color of button
+document.addEventListener('DOMContentLoaded', async function(){
+    const recipeData = await loadJSON()
+    const recipeCount = recipeData.length
+
+    createCard(recipeCount, recipeData)
+    heartInteractions()
+    bookmarkInteraction()    
+    copyLinkInteraction()
+});
+
+// load json file in recipe information
+async function loadJSON() {
+    try {
+        const response = await fetch('recipe_data.json');
+        const jsonData = await response.json();
+        return jsonData
+    } catch (error) {
+        console.error('Error loading JSON file:', error)
+    }
+
+}
+
+// create duplicate cards of the card template
+function createCard(amount, data) {
+    const cardTemplate = document.querySelector('.card')    
+    cardTemplate.style.display = 'none'
+    nthCard = amount + 1
+    for (let i = 0; i < amount; i++) {
+        nthCard -= 1
+        const cardClone = cardTemplate.cloneNode(true)
+        cardTemplate.parentNode.insertBefore(cardClone, cardTemplate.nextSibling)
+        cardClone.style.display = 'block'
+        updateCardInformation(cardClone, nthCard, data)
+    }
+}
+
+// update information in cards
+function updateCardInformation(cardClone, number, data) {
+    const recipeDetails = data[number-1]
+    const {
+        recipe_link,
+        recipe_image,
+        recipe_title,
+        recipe_description,
+        prep_time,
+        allergens,
+        cooking_skill_level,
+        cuisine_type,
+    } = recipeDetails;
+
+    const elementIdToUpdate = cardClone.querySelectorAll('[id]')
+    for (const element of elementIdToUpdate){
+        let currentId = element.id;
+        element.id = `${currentId}-${number.toString()}`
+        console.log(element)
+    }
+
+    const recipeLink = document.getElementById(`link-${number}`)
+    recipeLink.href = recipe_link
+
+    const recipePictureContainer = document.getElementById(`recipe-image-${number}`)
+    const recipeImageSource = recipePictureContainer.querySelectorAll('source')
+    const recipeImage = recipePictureContainer.querySelector('img')
+    recipeImageSource.forEach(source =>{
+        const mediaQuery = source.getAttribute('media')
+
+        if (mediaQuery === "(min-width: 582px)") {
+            source.scrset = recipe_image
+        } else if (mediaQuery === "(max-width: 581px)") {
+            source.scrset = recipe_image
+        }
+    })
+    recipeImage.src = recipe_image
+
+    const recipeTitle = document.getElementById(`recipe-title-${number}`).querySelector('a')
+    recipeTitle.textContent = recipe_title
+
+    const description = document.getElementById(`description-${number}`)
+    description.textContent = recipe_description
+
+    const prepTime = document.getElementById(`prep-time-${number}`)
+    prepTime.textContent = prep_time
+    
+    const recipeAllergens = document.getElementById(`allergens-${number}`)
+    recipeAllergens.textContent = allergens
+
+    const skillLevel = document.getElementById(`cooking-skill-${number}`)
+    skillLevel.textContent = cooking_skill_level
+
+    const foodCategory = document.getElementById(`food-category-${number}`)
+    foodCategory.textContent = cuisine_type
+}
+
+// adds a amount of like when recipe is liked - change  color of button
+function heartInteractions() {
     const heartButton = document.querySelectorAll('.heart');
-    let heartIdNumber = 0
+    let heartIdNumber = -1
     
     heartButton.forEach(heartButton => {
         heartIdNumber += 1
-        
         let heartIdString = heartIdNumber.toString()
-        let heartIdLikes = 'heartLikes-'+ heartIdString
         let heartIdColor = 'heartColor-' + heartIdString
+        let heartIdLikes = 'heartLikes-'+ heartIdNumber
         
         let heartNumber = randomInt(1,1000)
         const heartColor = document.getElementById(heartIdColor);
         const heartNumberDisplay = document.getElementById(heartIdLikes);
-               
-        heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
+
+        try{
+            heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
+        } catch(error) {
+            // pass
+        }
 
         heartButton.addEventListener('click', function(){
             likeChange = changeColor(heartColor, 'rgb(170, 26, 26)', 'heart')
             heartNumber += likeChange
             heartNumberDisplay.textContent = heartNumber.toLocaleString(); 
         })
-    });
 
+    });
+}
+
+function bookmarkInteraction() {
     // change color of button when clicked
     const bookmarkButton = document.querySelectorAll('.bookmark');
-    let bookmarkIdNumber = 0
+    let bookmarkIdNumber = -1
 
     bookmarkButton.forEach(bookmarkButton => {
         bookmarkIdNumber += 1;
@@ -38,42 +137,34 @@ document.addEventListener('DOMContentLoaded', function(){
             changeColor(bookmarkColor, 'rgb(230, 230, 42)', 'bookmark')
         });
     });
+}
 
+function copyLinkInteraction() {
     // open share menu pop up
     const copyButton = document.querySelectorAll('.copy-button')
-    let linkIdNumber = 0
+    let linkIdNumber = -1
     copyButton.forEach(copyButton => {
         linkIdNumber += 1
         let linkIdString = linkIdNumber.toString()
         let linkId = 'link-' + linkIdString
         let linkElement = document.getElementById(linkId)
-        let recipeLink = linkElement.getAttribute('href')
+
+        try{
+            let recipeLink = linkElement.getAttribute('href') 
+            let tempAnchor = document.createElement('a')
+            tempAnchor.href = recipeLink
+            let newRecipeLink = tempAnchor.href
+    
+            copyButton.addEventListener('click', function(){
+                copyLink(newRecipeLink)
+            })
+        } catch(error) {
+         
+        }
 
         // gets the link of the target html file. with this method, it capture the bitbucket.io as well instead of only the pure html href
-        let tempAnchor = document.createElement('a')
-        tempAnchor.href = recipeLink
-        let newRecipeLink = tempAnchor.href
-
-        copyButton.addEventListener('click', function(){
-            copyLink(newRecipeLink)
-        })
     });
-   
-    // close share menu pop up
-    const exitShare = document.querySelectorAll('.hide-bg-content, .close-share')
-    const sharePopup = document.querySelectorAll('.share-popup, .hide-bg-content');
-    exitShare.forEach(exitShare => {
-        exitShare.addEventListener('click', function(){
-            sharePopup.forEach(sharePopup => {
-                sharePopup.style.opacity = 0
-                setTimeout(() => {
-                    sharePopup.style.visibility = 'hidden' 
-                }, 200);
-            });
-        })
-    });
-
-});
+}
 
 // copies the link of the target url
 function copyLink(link){
