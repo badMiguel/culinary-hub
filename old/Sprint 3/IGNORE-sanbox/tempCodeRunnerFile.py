f09ce602acfb518a774eab6677daa8dc1c4d67af
MiguelBad <badayosbiboy@gmail.com>
2024-10-24 22:42:01 +0930

    cleanup

--- old/Sprint 3/IGNORE-sanbox/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-word = 'Hawaiian pizza is a classic pizza topped with a combination of tomato sauce and melted cheese, topped with slices of juicy ham, and the infamous pineapple.'
-
-word.split()
-print(word)
--- old/Sprint 3/IGNORE-sanbox/test.py
+++
@@ -1,7 +0,0 @@
-word = 'Hawaiian pizza is a classic pizza topped with a combination of tomato sauce and melted cheese, topped with slices of juicy ham, and the infamous pineapple.'
-
-total_word = 0
-for word in word:
-    total_word +=1
-    
-print(total_word)
